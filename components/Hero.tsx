35afa777b0d04560e379c25f7dd2ee381d32b37e
shohan-pherones <shohan.sub.56@gmail.com>
2023-05-29 18:23:55 +0600

    hero slider done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,67 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay, Pagination, Parallax } from "swiper";
+import { data } from "@/data/sliderImages";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 const Hero = () => {
-  return <section>Hero</section>;
+  return (
+    <section className="w-full h-[75vh]">
+      <Swiper
+        parallax={true}
+        pagination={{
+          clickable: true,
+        }}
+        speed={750}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        grabCursor={true}
+        navigation={true}
+        modules={[Navigation, Autoplay, Pagination, Parallax]}
+        className="mySwiper w-full h-full"
+      >
+        {data.map((item) => (
+          <SwiperSlide key={item.id}>
+            <div className="relative w-full h-full">
+              <Image
+                priority
+                src={item.src}
+                alt={item.alt}
+                width={1920}
+                height={1080}
+                className="w-full h-full object-cover"
+              />
+              <div className="absolute z-[1] top-0 left-0 w-full h-full bg-gradient-to-t from-black/75 to-transparent"></div>
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-center z-[2] space-y-5">
+                <h1
+                  data-swiper-parallax="-100%"
+                  className="text-8xl font-semibold"
+                >
+                  {item.heading}
+                </h1>
+                <p data-swiper-parallax="-200%" className="text-xl">
+                  {item.paragraph}
+                </p>
+                <div data-swiper-parallax="-300%">
+                  <Link href="/foods" className="btn btn-accent">
+                    {item.cta}
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
 };
 
 export default Hero;
---
+++ data/sliderImages.ts
@@ -0,0 +1,47 @@
+export const data = [
+  {
+    id: 1,
+    src: "https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg",
+    alt: "Fries and Burger on Plate",
+    heading: "Delicious Fries and Burger",
+    paragraph:
+      "Indulge in our mouthwatering fries and burger combo. Satisfy your cravings with the perfect blend of flavors and textures.",
+    cta: "Order Now",
+  },
+  {
+    id: 2,
+    src: "https://images.pexels.com/photos/1566837/pexels-photo-1566837.jpeg",
+    alt: "Shallow Focus Photography of Several Pizzas",
+    heading: "Authentic Pizzas",
+    paragraph:
+      "Experience the taste of Italy with our authentic pizzas made from the finest ingredients. Each bite will transport you to pizza heaven.",
+    cta: "Try It Now",
+  },
+  {
+    id: 3,
+    src: "https://images.pexels.com/photos/7758254/pexels-photo-7758254.jpeg",
+    alt: "Oriental noodle soup with herbs and chopsticks",
+    heading: "Savor the Oriental Delight",
+    paragraph:
+      "Indulge in a bowl of our flavorful Oriental noodle soup, garnished with fresh herbs and served with chopsticks. A culinary journey awaits you.",
+    cta: "Explore Menu",
+  },
+  {
+    id: 4,
+    src: "https://images.pexels.com/photos/5907592/pexels-photo-5907592.jpeg",
+    alt: "Happy Asian women cooking oriental noodles in kitchen",
+    heading: "Delightful Cooking",
+    paragraph:
+      "Experience the joy of cooking with our collection of authentic recipes and expert tips. Elevate your culinary skills and create mouthwatering dishes at home.",
+    cta: "Discover Recipes",
+  },
+  {
+    id: 5,
+    src: "https://images.pexels.com/photos/5718071/pexels-photo-5718071.jpeg",
+    alt: "Cutting Roasted Chicken in Close Up",
+    heading: "Succulent Roasted Chicken",
+    paragraph:
+      "Experience the juiciest and most flavorful roasted chicken in town. Our secret recipe and tender cooking process will leave you craving for more.",
+    cta: "Order Now",
+  },
+];
