2209219a078055f8a1b1efd999317c4d0aecbbd1
shohan-pherones <shohan.sub.56@gmail.com>
2023-05-31 03:27:05 +0600

    page cover added

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,5 +1,29 @@
+import Cover from "@/components/Cover";
+import PopularMenu from "@/components/PopularMenu";
+
 const MenuPage = () => {
-  return <main>MenuPage</main>;
+  return (
+    <main>
+      <Cover
+        image="https://images.pexels.com/photos/1049626/pexels-photo-1049626.jpeg"
+        title="Our Menu"
+        paragraph="Savor an exquisite culinary experience with our thoughtfully crafted menu, featuring a harmonious blend of flavors and inspired creations that will leave you craving for another bite."
+      />
+      <PopularMenu />
+      <Cover
+        image="https://images.pexels.com/photos/1049626/pexels-photo-1049626.jpeg"
+        title="Our Menu"
+        paragraph="Savor an exquisite culinary experience with our thoughtfully crafted menu, featuring a harmonious blend of flavors and inspired creations that will leave you craving for another bite."
+      />
+      <PopularMenu />
+      <Cover
+        image="https://images.pexels.com/photos/1049626/pexels-photo-1049626.jpeg"
+        title="Our Menu"
+        paragraph="Savor an exquisite culinary experience with our thoughtfully crafted menu, featuring a harmonious blend of flavors and inspired creations that will leave you craving for another bite."
+      />
+      <PopularMenu />
+    </main>
+  );
 };
 
 export default MenuPage;
---
+++ components/Cover.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Parallax } from "react-parallax";
+
+interface CoverProps {
+  image: string;
+  title: string;
+  paragraph?: string;
+}
+
+const Cover: React.FC<CoverProps> = ({ image, title, paragraph }) => {
+  return (
+    <Parallax
+      blur={{ min: -15, max: 15 }}
+      bgImage={image}
+      bgImageAlt={title}
+      strength={-200}
+    >
+      <section className="hero h-[50vh]">
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-xl">
+            <h1 className="mb-5 text-6xl font-bold">{title}</h1>
+            <p className="mb-5 text-lg">{paragraph}</p>
+          </div>
+        </div>
+      </section>
+    </Parallax>
+  );
+};
+
+export default Cover;
